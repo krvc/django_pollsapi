3c0d1f8977cb0372225aaf29f366a79fd3166d0f
rakesh <krvc.it@gmail.com>
2015-04-21 13:34:29 +0530

    Register an user through API

--- pollsapi/serializers.py
+++ pollsapi/serializers.py
@@ -24,10 +24,17 @@ class PollSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
-    polls = \
-        serializers.PrimaryKeyRelatedField(many=True,
-                                           queryset=Poll.objects.all())
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'polls')
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
