cb51b7aeabf83fb6d2e1cdec9293785bd81352da
rakesh <krvc.it@gmail.com>
2015-04-22 15:06:49 +0530

    Created Vote model

--- pollsapi/models.py
+++ pollsapi/models.py
@@ -5,17 +5,25 @@ from django.contrib.auth.models import User
 class Poll(models.Model):
     question = models.CharField(max_length=100)
     created_by = models.ForeignKey(User)
-    pub_date = models.DateTimeField()
-    owner = models.ForeignKey('auth.User', related_name='polls')
-
+    pub_date = models.DateTimeField(auto_now=True)
+    
     def __unicode__(self):
         return self.question
 
 
 class Choice(models.Model):
-    poll = models.ForeignKey(Poll, related_name='choice')
+    poll = models.ForeignKey(Poll, related_name='choices')
     choice_text = models.CharField(max_length=100)
-    vote = models.IntegerField(default=0)
+
 
     def __unicode__(self):
         return self.choice_text
+
+
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice, related_name='votes')
+    poll = models.ForeignKey(Poll)
+    voted_by = models.ForeignKey(User)
+
+    class Meta:
+        unique_together = ("poll", "voted_by")
