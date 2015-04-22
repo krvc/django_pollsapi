28d5e9764eb93c702f51dc0ea6dfe92009314bf4
rakesh <krvc.it@gmail.com>
2015-04-22 15:18:44 +0530

    Updated Choice and create vote Urls

--- django_pollsapi/urls.py
+++ django_pollsapi/urls.py
@@ -4,15 +4,13 @@ from django.contrib import admin
 import pollsapi.views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'django_pollsapi.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/', pollsapi.views.PollList.as_view()),
-    url(r'poll/(?P<pk>[0-9]+)/$', pollsapi.views.PollDetail.as_view()),
-    url(r'^users/$', pollsapi.views.UserList.as_view()),
+    url(r'^polls/$', pollsapi.views.PollList.as_view()),
+    url(r'polls/(?P<pk>[0-9]+)/$', pollsapi.views.PollDetail.as_view()),
+    url(r'^create_user/$', pollsapi.views.UserCreate.as_view()),
+    url(r'^choices/(?P<pk>[0-9]+)/$', pollsapi.views.ChoiceDetail.as_view()),
+    url(r'^create_vote/$', pollsapi.views.CreateVote.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', pollsapi.views.UserDetail.as_view()),
-    # url(r'^login/', pollsapi.views.Login.as_view()),
 
 ]
