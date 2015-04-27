43801e66a066dc31668245f2141a3ff917ad0586
rakesh <krvc.it@gmail.com>
2015-04-28 00:02:43 +0530

    Added global authentication settings

--- django_pollsapi/settings.py
+++ django_pollsapi/settings.py
@@ -98,6 +98,13 @@ USE_L10N = True
 USE_TZ = True
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+    )
+}
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
